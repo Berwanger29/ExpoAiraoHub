6ca379f4506d997a276f146da39ec50e2edf0da7
Vinicius Berwanger <bwg@Air-de-Vinicius.clickip>
2022-11-25 17:26:22 -0400

    texts of restaurants remake

--- privacyPolicy.js
+++ privacyPolicy.js
@@ -1,110 +1,48 @@
-export const privacyText = `
-Política de Privacidade \n
-No aplicativo de Turismo da cidade de Novo Airão, o AIRÃO HUB, privacidade e segurança são prioridades e nos comprometemos com a transparência do tratamento de dados pessoais dos nossos usuários/clientes. Por isso, esta presente Política de Privacidade estabelece como é feita a coleta, uso e transferência de informações de clientes ou outras pessoas que acessam ou usam nosso site.
-Ao utilizar nossos serviços, você entende que coletaremos e usaremos suas informações pessoais nas formas descritas nesta Política, sob as normas da Constituição Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), das normas de Proteção de Dados (LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis.
-Dessa forma, a (nome empresarial), doravante denominada simplesmente como “(nome empresarial simplificado)”, inscrita no CNPJ/MF sob o nº (nº do CNPJ), no papel de Controladora de Dados, obriga-se ao disposto na presente Política de Privacidade.
-1. Quais dados coletamos sobre você e para qual finalidade?
-Nosso site coleta e utiliza alguns dados pessoais seus, de forma a viabilizar a prestação de serviços e aprimorar a experiência de uso.
-1.1. Dados pessoais fornecidos pelo titular
-    •  Dado e finalidade
-    •  Dado e finalidade
-    •  Dado e finalidade
-    •  Dado e finalidade
-1.2. Dados pessoais coletados automaticamente
-    •  Dado e finalidade
-    •  Dado e finalidade
-    •  Dado e finalidade
-    •  Dado e finalidade
-2. Como coletamos os seus dados?
-Nesse sentido, a coleta dos seus dados pessoais ocorre da seguinte forma:
-    •  Como se coleta
-    •  Como se coleta
-    •  Como se coleta
-    •  Como se coleta
-2.1. Consentimento
-É a partir do seu consentimento que tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca pela qual você autoriza a (nome empresarial simplificado) a tratar seus dados.
-Assim, em consonância com a Lei Geral de Proteção de Dados, seus dados só serão coletados, tratados e armazenados mediante prévio e expresso consentimento. 
-O seu consentimento será obtido de forma específica para cada finalidade acima descrita, evidenciando o compromisso de transparência e boa-fé da (nome empresarial simplificado) para com seus usuários/clientes, seguindo as regulações legislativas pertinentes.
-Ao utilizar os serviços da (nome empresarial simplificado) e fornecer seus dados pessoais, você está ciente e consentindo com as disposições desta Política de Privacidade, além de conhecer seus direitos e como exercê-los.
-A qualquer tempo e sem nenhum custo, você poderá revogar seu consentimento.
-É importante destacar que a revogação do consentimento para o tratamento dos dados pode implicar a impossibilidade da performance adequada de alguma funcionalidade do site que dependa da operação. Tais consequências serão informadas previamente.
-3. Quais são os seus direitos?
-A (nome empresarial simplificado) assegura a seus usuários/clientes seus direitos de titular previstos no artigo 18 da Lei Geral de Proteção de Dados. Dessa forma, você pode, de maneira gratuita e a qualquer tempo:
-    • Confirmar a existência de tratamento de dados, de maneira simplificada ou em formato claro e completo.
-    • Acessar seus dados, podendo solicitá-los em uma cópia legível sob forma impressa ou por meio eletrônico, seguro e idôneo.
-    • Corrigir seus dados, ao solicitar a edição, correção ou atualização destes.
-    • Limitar seus dados quando desnecessários, excessivos ou tratados em desconformidade com a legislação através da anonimização, bloqueio ou eliminação.
-    • Solicitar a portabilidade de seus dados, através de um relatório de dados cadastrais que a (nome empresarial simplificado) trata a seu respeito.
-    • Eliminar seus dados tratados a partir de seu consentimento, exceto nos casos previstos em lei.
-    • Revogar seu consentimento, desautorizando o tratamento de seus dados.
-    • Informar-se sobre a possibilidade de não fornecer seu consentimento e sobre as consequências da negativa.
-4. Como você pode exercer seus direitos de titular?
-Para exercer seus direitos de titular, você deve entrar em contato com a (nome empresarial simplificado) através dos seguintes meios disponíveis:
-    •  Meio de contato
-    •  Meio de contato
-De forma a garantir a sua correta identificação como titular dos dados pessoais objeto da solicitação, é possível que solicitemos documentos ou demais comprovações que possam comprovar sua identidade. Nessa hipótese, você será informado previamente.
-5. Como e por quanto tempo seus dados serão armazenados?
-Seus dados pessoais coletados pela (nome empresarial simplificado) serão utilizados e armazenados durante o tempo necessário para a prestação do serviço ou para que as finalidades elencadas na presente Política de Privacidade sejam atingidas, considerando os direitos dos titulares dos dados e dos controladores.
-De modo geral, seus dados serão mantidos enquanto a relação contratual entre você e a (nome empresarial simplificado) perdurar. Findado o período de armazenamento dos dados pessoais, estes serão excluídos de nossas bases de dados ou anonimizados, ressalvadas as hipóteses legalmente previstas no artigo 16 lei geral de proteção de dados, a saber:
-I – cumprimento de obrigação legal ou regulatória pelo controlador;
-II – estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização dos dados pessoais;
-III – transferência a terceiro, desde que respeitados os requisitos de tratamento de dados dispostos nesta Lei; ou
-IV – uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que anonimizados os dados.
-Isto é, informações pessoais sobre você que sejam imprescindíveis para o cumprimento de determinações legais, judiciais e administrativas e/ou para o exercício do direito de defesa em processos judiciais e administrativos serão mantidas, a despeito da exclusão dos demais dados. 
-O armazenamento de dados coletados pela (nome empresarial simplificado) reflete o nosso compromisso com a segurança e privacidade dos seus dados. Empregamos medidas e soluções técnicas de proteção aptas a garantir a confidencialidade, integridade e inviolabilidade dos seus dados. Além disso, também contamos com medidas de segurança apropriadas aos riscos e com controle de acesso às informações armazenadas.
-6. O que fazemos para manter seus dados seguros?
-Para mantermos suas informações pessoais seguras, usamos ferramentas físicas, eletrônicas e gerenciais orientadas para a proteção da sua privacidade.
-Aplicamos essas ferramentas levando em consideração a natureza dos dados pessoais coletados, o contexto e a finalidade do tratamento e os riscos que eventuais violações gerariam para os direitos e liberdades do titular dos dados coletados e tratados.
-Entre as medidas que adotamos, destacamos as seguintes:
-    • Apenas pessoas autorizadas têm acesso a seus dados pessoais
-    • O acesso a seus dados pessoais é feito somente após o compromisso de confidencialidade
-    • Seus dados pessoais são armazenados em ambiente seguro e idôneo.
-A (nome empresarial simplificado) se compromete a adotar as melhores posturas para evitar incidentes de segurança. Contudo, é necessário destacar que nenhuma página virtual é inteiramente segura e livre de riscos. É possível que, apesar de todos os nossos protocolos de segurança, problemas de culpa exclusivamente de terceiros ocorram, como ataques cibernéticos de hackers, ou também em decorrência da negligência ou imprudência do próprio usuário/cliente.
-Em caso de incidentes de segurança que possa gerar risco ou dano relevante para você ou qualquer um de nossos usuários/clientes, comunicaremos aos afetados e a Autoridade Nacional de Proteção de Dados sobre o ocorrido, em consonância com as disposições da Lei Geral de Proteção de Dados.
-7. Com quem seus dados podem ser compartilhados?
-Tendo em vista a preservação de sua privacidade, a (nome empresarial simplificado) não compartilhará seus dados pessoais com nenhum terceiro não autorizado. 
-Seus dados poderão ser compartilhados com nossos parceiros comerciais: (nome completo ou empresarial do parceiro comercial), inscrito no CPF/CNPJ sob o nº (número do CPF Ou CNPJ do parceiro comercial).
-Estes recebem seus dados apenas na medida do necessário para a prestação dos serviços contratados e nossos contratos são orientados pelas normas de proteção de dados do ordenamento jurídico brasileiro.
-Todavia, nossos parceiros têm suas próprias Políticas de Privacidade, que podem divergir desta. Recomendamos a leitura desses documentos, que você pode acessar aqui:
-Política de Privacidade do nosso parceiro: (link para a política de privacidade do parceiro comercial).
-Além disso, também existem outras hipóteses em que seus dados poderão ser compartilhados, que são:
-I – Determinação legal, requerimento, requisição ou ordem judicial, com autoridades judiciais, administrativas ou governamentais competentes.
-II – Caso de movimentações societárias, como fusão, aquisição e incorporação, de forma automática
-III – Proteção dos direitos da (nome empresarial simplificado) em qualquer tipo de conflito, inclusive os de teor judicial.
-7.1. Transferência internacional de dados
-Alguns dos terceiros com quem compartilhamos seus dados podem ser localizados ou ou possuir instalações localizadas em países estrangeiros. Nessas condições, de toda forma, seus dados pessoais estarão sujeitos à Lei Geral de Proteção de Dados e às demais legislações brasileiras de proteção de dados
-Nesse sentido, a (nome empresarial simplificado) se compromete a sempre adotar eficientes padrões de segurança cibernética e de proteção de dados, nos melhores esforços de garantir e cumprir as exigências legislativas.
-Ao concordar com essa Política de Privacidade, você concorda com esse compartilhamento, que se dará conforme as finalidades descritas no presente instrumento.
-8. Cookies ou dados de navegação
-A (nome empresarial simplificado) faz uso de Cookies, que são arquivos de texto enviados pela plataforma ao seu computador e que nele se armazenam, que contém informações relacionadas à navegação do site. Em suma, os Cookies são utilizados para aprimorar a experiência de uso.
-Ao acessar nosso site e consentir com o uso de Cookies, você manifesta conhecer e aceitar a utilização de um sistema de coleta de dados de navegação com o uso de Cookies em seu dispositivo.
-A (nome empresarial simplificado) utiliza os seguintes Cookies: (descrição dos tipos de Cookies utilizados pelo site).
-Você pode, a qualquer tempo e sem nenhum custo, alterar as permissões, bloquear ou recusar os Cookies. Todavia, a revogação do consentimento de determinados Cookies pode inviabilizar o funcionamento correto de alguns recursos da plataforma.
-Para gerenciar os cookies do seu navegador, basta fazê-lo diretamente nas configurações do navegador, na área de gestão de Cookies. Você pode acessar tutoriais sobre o tema diretamente nos links abaixo:
-Se você usa o Internet Explorer.
-Se você usa o Firefox.
-Se você usa o Safari.
-Se você usa o Google Chrome.
-Se você usa o Microsoft Edge.
-Se você usa o Opera.
-Você pode ter maiores informações sobre os Cookies que utilizamos e como eles funcionam na nossa Política de Cookies, disponível neste link (link para a Política de Cookies).
-9. Alteração desta Política de Privacidade
-A atual versão da Política de Privacidade foi formulada e atualizada pela última vez em: (data da última atualização da Política de Privacidade).
-Reservamos o direito de modificar essa Política de Privacidade a qualquer tempo, principalmente em função da adequação a eventuais alterações feitas em nosso site ou em âmbito legislativo. Recomendamos que você a revise com frequência.
-Eventuais alterações entrarão em vigor a partir de sua publicação em nosso site e sempre lhe notificaremos acerca das mudanças ocorridas.
-Ao utilizar nossos serviços e fornecer seus dados pessoais após tais modificações, você as consente. 
-10. Responsabilidade
-A (nome empresarial simplificado) prevê a responsabilidade dos agentes que atuam nos processos de tratamento de dados, em conformidade com os artigos 42 ao 45 da Lei Geral de Proteção de Dados.
-Nos comprometemos em manter esta Política de Privacidade atualizada, observando suas disposições e zelando por seu cumprimento.
-Além disso, também assumimos o compromisso de buscar condições técnicas e organizativas seguramente aptas a proteger todo o processo de tratamento de dados.
-Caso a Autoridade Nacional de Proteção de Dados exija a adoção de providências em relação ao tratamento de dados realizado pela (nome empresarial simplificado), comprometemo-nos a segui-las. 
-10.1 Isenção de responsabilidade
-Conforme mencionado no Tópico 6, embora adotemos elevados padrões de segurança a fim de evitar incidentes, não há nenhuma página virtual inteiramente livre de riscos. Nesse sentido, a (nome empresarial simplificado) não se responsabiliza por:
-I – Quaisquer consequências decorrentes da negligência, imprudência ou imperícia dos usuários em relação a seus dados individuais. Garantimos e nos responsabilizamos apenas pela segurança dos processos de tratamento de dados e do cumprimento das finalidades descritas no presente instrumento.
-Destacamos que a responsabilidade em relação à confidencialidade dos dados de acesso é do usuário.
-II – Ações maliciosas de terceiros, como ataques de hackers, exceto se comprovada conduta culposa ou deliberada da (nome empresarial simplificado).
-Destacamos que em caso de incidentes de segurança que possam gerar risco ou dano relevante para você ou qualquer um de nossos usuários/clientes, comunicaremos aos afetados e a Autoridade Nacional de Proteção de Dados sobre o ocorrido e cumpriremos as providências necessárias.
-III – Inveracidade das informações inseridas pelo usuário/cliente nos registros necessários para a utilização dos serviços da (nome empresarial simplificado); quaisquer consequências decorrentes de informações falsas ou inseridas de má-fé são de inteiramente responsabilidade do usuário/cliente.
-11. Encarregado de Proteção de Dados
-A (nome empresarial simplificado) disponibiliza os seguintes meios para que você possa entrar em contato conosco para exercer seus direitos de titular: (meios de contato).
-Caso tenha dúvidas sobre esta Política de Privacidade ou sobre os dados pessoais que tratamos, você pode entrar em contato com o nosso Encarregado de Proteção de Dados Pessoais, através dos seguintes canais:`
+`TERMO DE PRIVACIDADE
+
+Este SERVIÇO é fornecido pelo criador do App, sem custo e é destinado para informar os visitantes e usuários sobre as políticas de coleta, uso e divulgação de informações pessoais, caso alguém decida usar nossos serviços.
+
+Se você optar por usar nossos serviços, concordará com a coleta e uso de informações relacionadas a essa política. As informações pessoais coletadas por nós são usadas para fornecer e melhorar o serviço e não usaremos e nem compartilharemos seus dados com ninguém, exceto conforme descrito nesta Política de Privacidade.
+
+Os termos usados nesta Política de Privacidade têm os mesmos significados que os nossos Termos e Condições, que estão acessíveis no AIRÃO HUB, a menos que definido de outra forma nesta Política de Privacidade.
+
+COLETA E USO DE INFORMAÇÕES
+Para uma melhor experiência, ao usar nosso Serviço, nós podemos exigir que você nos forneça algumas informações pessoalmente identificáveis como seu e-mail, nome completo, CPF, telefone, data de nascimento, Gênero, Localização, além de informações comerciais, opiniões, textos, dados de meios de pagamento e outras mais. Ao utilizar nossos serviços, você concorda que coletaremos e usaremos suas informações fornecidas nas formas descritas nesta Política de Privacidade, de acordo com as normas da Constituição Federal de 1988 (art. 5º, LXXIX, e o art. 22, XXX), com o disposto na Lei Geral de Proteção de Dados, Lei Federal nº 13.709, de 14 de agosto de 2018, e com as disposições consumeristas da Lei Federal nº 8.078, de 11 de setembro de 1990, assim como demais normas do ordenamento jurídico brasileiro aplicáveis.
+O aplicativo usa serviços de terceiros que podem coletar informações usadas para identifica-lo(a).
+•	Google Play Services
+•	Facebook
+•	Google Maps
+•	Youtube
+•	Twitter
+•	Instagram
+Queremos informá-lo (a) que sempre que você usar os nossos serviços, no caso de um erro no aplicativo, coletamos dados e informações (por meio de produtos de terceiros) em seu telefone, chamados Dados de Registro. Esses dados de registro podem incluir informações como endereço de IP do dispositivo, versão do sistema operacional, configuração do aplicativo ao utilizar os nossos serviços, a hora e a data do seu uso do serviços, a hora e a data do seu uso do serviço e outras estatísticas.
+COOKIES
+
+Cookies são arquivos com uma pequena quantidade de dados que são comumente usados como identificadores exclusivos anônimos. Estes são enviados para o seu navegador a partir dos sites que você visita e são armazenados na memória interna do seu dispositivo.
+Este serviço não usa esses “cookies” explicitamente. No entanto, o aplicativo pode usar código de terceiros e bibliotecas que usam cookies para coletar informações e melhorar seus serviços. Você tem a opção de aceitar ou recusar esses cookies e saber quando um deles está sendo enviado para o seu dispositivo. Se você optar por recusar nossos cookies, talvez não consiga usar algumas partes deste serviço.
+
+PROVEDORES DE SERVIÇO
+
+Não podemos empregar empresas e indivíduos de terceiros devido às seguintes razões:
+•	Para facilitar nossos serviços;
+•	Para fornecer o serviço em nosso nome;
+•	Para executar serviços relacionados a serviço; ou
+•	Para nos ajudar a analisar como nosso Serviço é usado.
+Queremos informar aos usuários deste serviço que esses terceiros têm acesso às suas informações pessoais. O motivo é executar as tarefas atribuídas a eles em nosso nome. No entanto, eles são obrigados a não divulgar ou usar as informações para qualquer outra finalidade.
+
+SEGURANÇA
+
+Nós valorizamos sua confiança ao nos fornecer suas informações pessoais, assim, estamos nos esforçando para usar meios comercialmente aceitáveis de protege-los. Mas lembre-se de que nenhum método de transmissão pela internet, ou método de armazenamento eletrônico é 100% seguro e confiável, e nós não podemos garantir sua segurança absoluta.
+
+LINKS PARA OUTROS SITES
+
+Este serviço pode conter links, webviews e iframes para outros sites. Se você clicar em um link de terceiros e/ou acessar conteúdo desses sites, você será direcionado para esse site e/ou estará sujeito às políticas de privacidade dos mesmos. Observe que esses sites externos não são operados por nós. Portanto, aconselhamos fortemente que você revise a Política de Privacidade desses sites, pois não temos controle e não assumimos nenhuma responsabilidade pelo conteúdo, políticas de privacidade ou práticas de quaisquer sites ou serviços de terceiros.
+
+PRIVACIDADE INFANTIL
+
+Estes Serviços não tratam ninguém com idade inferior a 13 anos. Não coletamos intencionalmente informações pessoalmente identificáveis de crianças menores de 13 anos. Se no caso, descobrirmos que uma criança com menos de 13 anos forneceu com informações pessoais, excluiremos imediatamente isso de nossos servidores. Se você é pai/mãe ou responsável legal e sabe que seu filho (a) nos forneceu informações pessoais, entre em contato conosco para que possamos tornar as providências necessárias.
+
+CONTATE-NOS
+Através do e-mail seminturna15@gmail.com
+Se você tiver dúvidas ou sugestões sobre a nossa Política de Privacidade, não hesite em entrar em contato para melhorarmos os nossos serviços.
+`
